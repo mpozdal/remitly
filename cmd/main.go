44f61df5cacdb97a30c35cff108008dda7234911
Michal <pozdal.michal@gmail.com>
2025-01-30 22:42:04 +0100

    init db connection

--- cmd/main.go
+++ cmd/main.go
@@ -4,12 +4,41 @@ import (
 	"log"
 
 	"mpozdal/remitly/cmd/api"
+	"mpozdal/remitly/config"
+	"mpozdal/remitly/db"
+
+	"github.com/go-sql-driver/mysql"
 )
 
 func main() {
+
+	dbm, err := db.NewDBManager(mysql.Config{
+		User:                 config.Envs.DBUser,
+		Passwd:               config.Envs.DBPassword,
+		Addr:                 config.Envs.DBAddress,
+		DBName:               config.Envs.DBName,
+		Net:                  "tcp",
+		AllowNativePasswords: true,
+		ParseTime:            true,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	initDatabase(dbm)
 
 	server := api.NewAPIServer(":8080")
 	if err := server.Run(); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func initDatabase(dbm *db.DBManager) {
+	err := dbm.DB.Ping()
+	if err != nil {
+		log.Fatal(err)
+
+	}
+
+	log.Println("Database setup completed")
+
 }
--- db/db.go
+++ db/db.go
@@ -1,1 +1,20 @@
 package db
+
+import (
+	"database/sql"
+	"log"
+
+	"github.com/go-sql-driver/mysql"
+)
+
+type DBManager struct {
+	DB *sql.DB
+}
+
+func NewDBManager(cfg mysql.Config) (*DBManager, error) {
+	db, err := sql.Open("mysql", cfg.FormatDSN())
+	if err != nil {
+		log.Fatal(err)
+	}
+	return &DBManager{DB: db}, nil
+}
